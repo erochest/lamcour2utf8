1f011204949c2c7528cf3bb6e7e9672291beba1f
Eric Rochester <erochest@gmail.com>
2018-08-04 17:24:54 -0400

    Read and translate a single file.

--- src/cli.rs
+++ src/cli.rs
@@ -1,16 +1,44 @@
 use super::Result;
 use lamcour::translate_bytes;
+use std::env;
+use std::fs::File;
 use std::io::{self, Read, Write};
+use std::path::{Path, PathBuf};
 
 pub fn run() -> Result<()> {
+    let args = env::args().skip(1).collect::<Vec<_>>();
+
+    if args.len() == 0 {
+        process_stdin()
+    } else {
+        process_file(&PathBuf::from(&args[0]))
+    }
+}
+
+fn process_stdin() -> Result<()> {
+    process(&mut io::stdin(), &mut io::stdout())
+}
+
+fn process_file(path: &Path) -> Result<()> {
+    let mut input =
+        File::open(path).map_err(|err| format_err!("Unable to open file {:?}: {:?}", path, &err))?;
+
+    let output_path = path.with_extension("utf8");
+    let mut output = File::create(&output_path)
+        .map_err(|err| format_err!("Unable to create file {:?}: {:?}", &output_path, &err))?;
+
+    process(&mut input, &mut output)
+}
+
+fn process<R: Read, W: Write>(input: &mut R, output: &mut W) -> Result<()> {
     let mut buffer = Vec::new();
-    io::stdin()
+    input
         .read_to_end(&mut buffer)
         .map_err(|err| format_err!("Error reading from STDIN: {:?}", &err))?;
 
     let contents = translate_bytes(buffer.as_slice());
 
-    io::stdout()
+    output
         .write_all(contents.as_bytes())
         .map_err(|err| format_err!("Error writing to STDOUT: {:?}", &err))
 }
---
+++ tests/file.rs
@@ -0,0 +1,29 @@
+extern crate assert_cmd;
+extern crate assert_fs;
+extern crate predicates;
+
+use assert_cmd::prelude::*;
+use assert_fs::prelude::*;
+use predicates::prelude::*;
+use std::path::Path;
+use std::process::Command;
+
+#[test]
+fn test_processes_file() {
+    let temp = assert_fs::TempDir::new().unwrap();
+    temp.copy_from("./tests/fixtures/input", &["dragonfly_"])
+        .unwrap();
+    let dragonfly = temp.child("dragonfly_");
+    let dragonfly_utf8 = temp.child("dragonfly_.utf8");
+
+    let command = Command::main_binary()
+        .unwrap()
+        .args(dragonfly.path().to_str())
+        .unwrap();
+
+    command.assert().success();
+
+    let predicate_file =
+        predicate::path::eq_file(Path::new("./tests/fixtures/expected/dragonfly_.utf8"));
+    dragonfly_utf8.assert(predicate_file);
+}
